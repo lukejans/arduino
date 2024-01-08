5909e2a8ff4700e40dc104656f1fa3661e12c0e9
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2024-01-08 15:40:03 -0500

    Change tilt switch variables to boolean and initialize in setup

--- sketch-book/digital-hourglass/digital-hourglass.ino
+++ sketch-book/digital-hourglass/digital-hourglass.ino
@@ -10,8 +10,8 @@ tilt switch
     - stating position (1)
 */ 
 const int switchPin = 8;
-int switchState = 0;
-int preSwitchState = 0;
+bool switchState;
+bool preSwitchState;
 
 // indicates the last time an led was turned on
 unsigned long previousTime = 0;
@@ -23,19 +23,25 @@ int led = 2;
 unsigned short interval = 4000;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(9600);             // debug
 
+    // set pin direction
     for (int x = 2; x < 8; x++) {
         pinMode(x, OUTPUT);
     }
     pinMode(switchPin, INPUT);
+
+    // initialize tilt switch variables
+    switchState = digitalRead(switchPin);
+    preSwitchState = switchState;
+
 }
 
 void loop() {
     // tracking time
     unsigned long currentTime = millis();
 
-    // evaluate the amount of time that has passed 
+    //  turn LEDs on 
     if (currentTime - previousTime > interval) {
         // track last LED turn on time
         previousTime = currentTime;
@@ -50,18 +56,17 @@ void loop() {
         }
     }
 
-    Serial.print("tilt switch: ");
-    Serial.println(switchState);
+    Serial.print("tilt switch: ");  // debug
+    Serial.println(switchState);    // debug
 
-    // check if the tilt switch changed state
     switchState = digitalRead(switchPin);
 
+    // check if the tilt switch changed state
     if (switchState != preSwitchState) {
         // reset hour glass
         for (int x = 2; x < 8; x++) {
             digitalWrite(x, LOW);
         }
-
         led = 2;
         previousTime = currentTime;
     }

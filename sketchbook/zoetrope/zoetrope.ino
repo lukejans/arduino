ac5054d2b4639c50ea7dda6fff71eeab3a25ea8c
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2024-01-10 01:34:20 -0500

    Document h-bridge usage

--- sketchbook/zoetrope/zoetrope.ino
+++ sketchbook/zoetrope/zoetrope.ino
@@ -1,5 +1,35 @@
+/*
+Project 10:
 
-void setup() {
+Zoetrope
+
+  - using H-bridge integrated circuit (IC)
+                    _   _
+        [PWM]   1 -|     |- 16   [PWR]
+       [SPIN]   2 -|  L  |- 15
+         [M-]   3 -|  2  |- 14
+        [GND]   4 -|  9  |- 13
+        [GND]   5 -|  3  |- 12
+         [M+]   6 -|  D  |- 11
+       [SPIN]   7 -|     |- 10
+        [PWR]   8 -|_____|- 9
+
+  [PWM]  
+    - PWM dc motor speed
+  [SPIN]
+    - communicate spin direction
+    - 2:HIGH 7:LOW = clockwise
+    - 2:LOW 7:HIGH = counter-clockwise
+    - if both pins are HIGH or both LOW = stop
+  [M]
+    - dc motor positive and negative direction
+  [PWR]
+     8: dc motor power
+    16: dc motor power
+*/
+
+void setup() {  
+
 }
 
 void loop() {

6ee4b03f648a5f9dc7793c6c52d21f8ecb1681c7
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2024-01-14 12:22:14 -0500

    Initial tweak-the-logo sketch serial communication with processing

--- sketchbook/tweak-the-logo/tweak-the-logo.ino
+++ sketchbook/tweak-the-logo/tweak-the-logo.ino
@@ -1,6 +1,15 @@
+/*
+Project 14:
+
+Tweak The Logo:
+    - serial communication with a computer program, Processing
+*/
 
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
+  Serial.write(analogRead(A0) / 4);
+  delay(1);
 }

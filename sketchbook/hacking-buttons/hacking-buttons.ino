9111ddce08ea7a3819b6c91141d4b81b5c228bda
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2024-01-22 11:53:47 -0500

    Add hacking-buttons sketch and circuits

---
+++ sketchbook/hacking-buttons/hacking-buttons.ino
@@ -0,0 +1,18 @@
+/*
+Project 15:
+
+Hacking Buttons
+
+  - using optocoupler
+
+         ______
+     1 -|      |- 6
+     2 -| 4N35 |- 5
+     3 -|______|- 4
+
+*/
+void setup() {
+}
+
+void loop() {
+}
